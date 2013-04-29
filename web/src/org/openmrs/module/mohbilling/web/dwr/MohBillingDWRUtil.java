4db8ad4bb21bf89ae3fe64df93ef19b8c9d472dd
kamonyo <kamonyo@6101c9c6-cf70-4d46-9f71-e9b75042c301>
2013-04-29 09:01:17 +0000

    Updated to 0.0.8 version: 	1. Having the Find Appointment by Patient Identifier working properly. 	2. Having only Waiting Appointments list on the Provider's appointments dashboard 	3. For those Upcoming Appointments they will be found on the Advanced Search page
    git-svn-id: http://emr.moh.gov.rw/svn/modules/moh-billing/branches/1.6.x@2371 6101c9c6-cf70-4d46-9f71-e9b75042c301

--- web/src/org/openmrs/module/mohbilling/web/dwr/MohBillingDWRUtil.java
+++ web/src/org/openmrs/module/mohbilling/web/dwr/MohBillingDWRUtil.java
@@ -15,13 +15,11 @@ import org.openmrs.module.mohbilling.model.Beneficiary;
 public class MohBillingDWRUtil {
 
 	public String getBeneficiaryListInTable(String searchString) {
-   		
-			//System.out.println(" hello am getting here and i can get a string name "+searchString);
-		
+
 		BeneficiaryListItem bli = null;
 
-		// perform a search in the db using existing service method & parameter
-		// searchString
+		/** perform a search in the db using existing service method & parameter
+			searchString */
 		Beneficiary ben = InsurancePolicyUtil
 				.getBeneficiaryByPolicyIdNo(searchString);
 
@@ -30,34 +28,32 @@ public class MohBillingDWRUtil {
 		else
 			return "";
 
-		// building the string to return, the string is a list/table containing
-		// beneficiaries
+		/** building the string to return, the string is a list/table containing
+		 beneficiaries*/
 		StringBuilder sb = new StringBuilder("");
-		sb
-				.append("<table class='openmrsSearchTable' cellpadding=2 cellspacing=0 style='width:100%; font-size:0.8em'>");
-		sb
-				.append("<tr><td colspan='10' style='text-align:right; font-style:italic;'>Result for &quot;"
-						+ searchString
-						+ "&quot;: "
-						+ ((ben != null) ? 1 : 0)
-						+ " beneficiaries</tr>");
+		sb.append("<table class='openmrsSearchTable' cellpadding=2 cellspacing=0 style='width:100%; font-size:0.8em'>");
+		sb.append("<tr><td colspan='10' style='text-align:right; font-style:italic;'>Result for &quot;"
+				+ searchString
+				+ "&quot;: "
+				+ ((ben != null) ? 1 : 0)
+				+ " beneficiaries</tr>");
 		sb.append("<tr class='oddRow'>" + "<th>#</th>"
-				+ "<th>Insurance Policy No.</th>"
-				+ "<th>Insurance Card No.</th>" + "<th>Given Name</th>"
-				+ "<th>Middle Name</th>" + "<th>Family Name</th>"
+				+ "<th>Insurance Policy No.</th>" + "<th>Insurance</th>"
+				+ "<th>Insurance Card No.</th>" + "<th>Patient names</th>"
 				+ "<th>Age</th>" + "<th>Gender</th>" + "<th></th>"
 				+ "<th>Birthdate</th></tr>");
 
-		// the result should be listed here
+		/** the result should be listed here */
 
 		if (null != bli) {
 			sb.append("<tr class='searchRow' onclick=editInsurancePolicy('"
 					+ bli.getInsurancePolicyId() + "','" + searchString + "')>"
 					+ "<td>1.</td>" + "<td>" + bli.getInsurancePolicyNumber()
-					+ "</td>" + "<td>" + bli.getInsuranceCardNumber() + "</td>"
-					+ "<td>" + bli.getGivenName() + "</td>" + "<td>"
-					+ bli.getMiddleName() + "</td>" + "<td>"
-					+ bli.getFamilyName() + "</td>");
+					+ "<td>"
+					+ ben.getInsurancePolicy().getInsurance().getName()
+					+ "</td>" + "</td>" + "<td>" + bli.getInsuranceCardNumber()
+					+ "</td>" + "<td>" + bli.getGivenName() + " "
+					+ bli.getMiddleName() + " " + bli.getFamilyName() + "</td>");
 			sb.append("<td>"
 					+ ((bli.getAge().intValue() >= 1) ? bli.getAge() : "<1")
 					+ "</td>");
