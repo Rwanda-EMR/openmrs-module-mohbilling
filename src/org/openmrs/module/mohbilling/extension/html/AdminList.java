a6c393a66e6572b2889523e171043da4b0acb878
eric <eric@6101c9c6-cf70-4d46-9f71-e9b75042c301>
2014-08-27 10:29:49 +0000

    Update "Manage Reports" privilege to "Manage Billing Reports"
    git-svn-id: http://emr.moh.gov.rw/svn/modules/moh-billing/branches/1.6.x@2986 6101c9c6-cf70-4d46-9f71-e9b75042c301

--- src/org/openmrs/module/mohbilling/extension/html/AdminList.java
+++ src/org/openmrs/module/mohbilling/extension/html/AdminList.java
@@ -55,14 +55,14 @@ public class AdminList extends AdministrationSectionExt {
 		Map<String, String> map = new HashMap<String, String>();
 
 		if (Context.getAuthenticatedUser().hasPrivilege("Add Facility service"))
-			map.put("module/mohbilling/facilityService.list",	"mohbilling.facility.service.manage");
+			map.put("module/mohbilling/facilityService.list", "mohbilling.facility.service.manage");
 		if (Context.getAuthenticatedUser().hasPrivilege("Add Insurance"))
 			map.put("module/mohbilling/insurance.list", "mohbilling.insurance.manage");
 		if (Context.getAuthenticatedUser().hasPrivilege("Create Insurance Policy"))
-			map.put("module/mohbilling/insurancePolicySearch.form", 	"mohbilling.insurance.policy.manage");
+			map.put("module/mohbilling/insurancePolicySearch.form", "mohbilling.insurance.policy.manage");
 		if (Context.getAuthenticatedUser().hasPrivilege("Manage Patient Bill Calculations"))
 			map.put("module/mohbilling/patientSearchBill.form", "mohbilling.billing.manage");
-		if (Context.getAuthenticatedUser().hasPrivilege("Manage Reports"))
+		if (Context.getAuthenticatedUser().hasPrivilege("Manage Billing Reports"))
 			map.put("module/mohbilling/cohort.form", "mohbilling.billing.report");
 
 		return map;
