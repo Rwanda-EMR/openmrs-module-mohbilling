43b4155f32ef0741719b26207d3a2135e38c7295
mariam <mariam@6101c9c6-cf70-4d46-9f71-e9b75042c301>
2014-11-25 10:40:48 +0000

    work on billCohortBuilder and paymentsCohortBuilder methods for "Find bills", "Payments" and "Check bill payment bill" links
    git-svn-id: http://emr.moh.gov.rw/svn/modules/moh-billing/branches/1.6.x@3046 6101c9c6-cf70-4d46-9f71-e9b75042c301

--- web/src/org/openmrs/module/mohbilling/web/controller/MohBIllingrReceivedAmountController.java
+++ web/src/org/openmrs/module/mohbilling/web/controller/MohBIllingrReceivedAmountController.java
@@ -3,24 +3,27 @@
  */
 package org.openmrs.module.mohbilling.web.controller;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.velocity.runtime.directive.Parse;
 import org.hibernate.SessionFactory;
+import org.openmrs.Patient;
 import org.openmrs.User;
 import org.openmrs.api.context.Context;
-import org.openmrs.module.mohbilling.businesslogic.BillPaymentUtil;
+import org.openmrs.module.mohbilling.businesslogic.InsuranceUtil;
+import org.openmrs.module.mohbilling.businesslogic.ReportsUtil;
 import org.openmrs.module.mohbilling.model.BillPayment;
+import org.openmrs.module.mohbilling.model.Insurance;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.ParameterizableViewController;
-
-import java.math.BigDecimal;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.*;
 
 public class MohBIllingrReceivedAmountController extends
 		ParameterizableViewController {
@@ -32,55 +35,130 @@ public class MohBIllingrReceivedAmountController extends
 	protected ModelAndView handleRequestInternal(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
 
+		List<String> categories = InsuranceUtil.getAllServiceCategories();
+		List<BillPayment> reportedPayments = new ArrayList<BillPayment>();
+
 		ModelAndView mav = new ModelAndView();
-		String startDateStr = null;
-		String endDateStr = null;
-		String billCollector = null;
-		Date startDate = null;
-		Date endDate = null ;
-		User collector = null;
-		BillPaymentUtil billPaymentUtil = null;
-		DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+		mav.addObject("allInsurances", InsuranceUtil.getAllInsurances());
+		mav.addObject("categories", categories);
 
-		if (request.getParameter("startDate") != null && request.getParameter("endDate") != null
-				&& request.getParameter("billCollector") != null) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+	//	Date startDate = null;
+
+		if (request.getParameter("patientId") != null
+				&& request.getParameter("insurance") != null
+				&& request.getParameter("startDate") != null
+				&& request.getParameter("endDate") != null
+				&& request.getParameter("serviceId") != null
+				&& request.getParameter("cashCollector") != null) {
+
+			String patientIdStr = request.getParameter("patientId"), insuranceStr = request
+					.getParameter("insurance"), startDateStr = request
+					.getParameter("startDate"), endDateStr = request
+					.getParameter("endDate"), serviceId = request
+					.getParameter("serviceId"), cashCollector = request
+					.getParameter("cashCollector");
 			
-			startDateStr = request.getParameter("startDate");
-			endDateStr = request.getParameter("endDate");
-
-			billCollector = request.getParameter("billCollector");
-
-			if (!startDateStr.equals("")) {
-				startDate = (Date) formatter.parse(startDateStr);
-			}
-			if (!endDateStr.equals("")) {
-				endDate = (Date) formatter.parse(endDateStr);
-			}
+			Date startDate = sdf.parse(startDateStr.split("/")[2] + "-"
+					+ startDateStr.split("/")[1] + "-"
+					+ startDateStr.split("/")[0]);
 			
-			Integer billCollectorId = null;
-             
-			if(billCollector != null && billCollector != ""){
-			billCollectorId = Integer.parseInt(billCollector);
-			collector = Context.getUserService().getUser(billCollectorId);
-             }
+			Date endDate = sdf.parse(endDateStr.split("/")[2] + "-"
+					+ endDateStr.split("/")[1] + "-"
+					+ endDateStr.split("/")[0]);
 
-			@SuppressWarnings("static-access")
-			List<BillPayment> billPaymentsByDateAndCollector = billPaymentUtil
-					.getBillPaymentsByDateAndCollector(startDate,endDate, collector);
+			Integer insuranceIdInt = null;
+			Integer patientId = null;
+	//		Date endDate = null;
+			Insurance insurance = null;
+			String patientNames = null;
 
-			mav.addObject("billPaymentsByDateAndCollector",
-					billPaymentsByDateAndCollector);
+			User user = Context.getAuthenticatedUser();
 
+			String cashierNames = (user.getPersonName().getFamilyName() != null ? user
+					.getPersonName().getFamilyName() : "")
+					+ " "
+					+ (user.getPersonName().getMiddleName() != null ? user
+							.getPersonName().getMiddleName() : "")
+					+ " "
+					+ (user.getPersonName().getGivenName() != null ? user
+							.getPersonName().getGivenName() : "");
+
+//			if (!startDateStr.equals("")) {
+//				startDate = (Date) formatter.parse(startDateStr);
+//			}
+//
+//			if (!endDateStr.equals("")) {
+//				endDate = (Date) formatter.parse(endDateStr);
+//			}
+
+			if (!request.getParameter("patientId").equals("")) {
+
+				patientIdStr = request.getParameter("patientId");
+				patientId = Integer.parseInt(patientIdStr);
+				Patient patient = Context.getPatientService().getPatient(
+						patientId);
+				patientNames = (patient.getFamilyName() != null ? patient
+						.getFamilyName() : "")
+						+ " "
+						+ (patient.getMiddleName() != null ? patient
+								.getMiddleName() : "")
+						+ " "
+						+ (patient.getGivenName() != null ? patient
+								.getGivenName() : "");
+			}
+
+			if (!request.getParameter("insurance").equals("")) {
+				insuranceIdInt = Integer.parseInt(insuranceStr);
+				insurance = InsuranceUtil.getInsurance(insuranceIdInt);
+			}
+
+			reportedPayments = ReportsUtil.paymentsCohortBuilder(insurance,
+					startDate, endDate, patientId, serviceId, null,
+					cashCollector);
+			
+			
+			
+			mav.addObject("reportedPayments", reportedPayments);
+			mav.addObject("startDateStr", startDateStr);
+			mav.addObject("endDateStr", endDateStr);
+			mav.addObject("serviceId", serviceId);
+			mav.addObject("insurance", insurance);
+			mav.addObject("patientId", patientId);
+			mav.addObject("cashCollector", cashCollector);
+			mav.addObject(
+					"healthFacilityName",
+					Context.getAdministrationService().getGlobalProperty(
+							"billing.healthFacilityName"));
+			mav.addObject("address", Context.getAdministrationService()
+					.getGlobalProperty("billing.healthFacilityPhysicalAddress"));
+			mav.addObject(
+					"healthFacilityShortCode",
+					Context.getAdministrationService().getGlobalProperty(
+							"billing.healthFacilityShortCode"));
+			mav.addObject(
+					"healthFacilityLogo",
+					Context.getAdministrationService().getGlobalProperty(
+							"billing.healthFacilityLogo"));
+			mav.addObject(
+					"healthFacilityEmail",
+					Context.getAdministrationService().getGlobalProperty(
+							"billing.healthFacilityEmail"));
+			mav.addObject("today", new Date());
+			mav.addObject("patientNames", patientNames);
+			mav.addObject("cashierNames", cashierNames);
+			
 			Double TotalReceivedAmount = (double) 0;
 
-			for (BillPayment billPayment : billPaymentsByDateAndCollector) {
-
+			for (BillPayment billPayment : reportedPayments) {
 				TotalReceivedAmount = TotalReceivedAmount
 						+ billPayment.getAmountPaid().doubleValue();
 
 			}
+			
             
 			mav.addObject("TotalReceivedAmount", TotalReceivedAmount); 
+		
 		}
 
 		mav.setViewName(getViewName());
