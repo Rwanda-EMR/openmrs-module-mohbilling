ebf8ee8bebc3fcc8896a5ed7ebb6e6c430b19a9c
mariam <mariam@6101c9c6-cf70-4d46-9f71-e9b75042c301>
2014-11-25 10:40:09 +0000

    work on billCohortBuilder and paymentsCohortBuilder methods for "Find bills", "Payments" and "Check bill payment bill" links
    git-svn-id: http://emr.moh.gov.rw/svn/modules/moh-billing/branches/1.6.x@3043 6101c9c6-cf70-4d46-9f71-e9b75042c301

--- web/src/org/openmrs/module/mohbilling/web/controller/MohBillingCheckPatientBillPaymentFormController.java
+++ web/src/org/openmrs/module/mohbilling/web/controller/MohBillingCheckPatientBillPaymentFormController.java
@@ -3,6 +3,8 @@
  */
 package org.openmrs.module.mohbilling.web.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,6 +16,7 @@ import org.openmrs.api.context.Context;
 import org.openmrs.module.mohbilling.businesslogic.InsurancePolicyUtil;
 import org.openmrs.module.mohbilling.businesslogic.PatientBillUtil;
 import org.openmrs.module.mohbilling.model.PatientBill;
+import org.openmrs.module.mohbilling.service.BillingService;
 import org.openmrs.web.WebConstants;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.ParameterizableViewController;
@@ -34,31 +37,54 @@ public class MohBillingCheckPatientBillPaymentFormController extends
 
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName(getViewName());
-
-		if (request.getParameter("patientId") != null)
-			if (!request.getParameter("patientId").equals("")) {
-				List<PatientBill> pbList = PatientBillUtil
-						.getBillsByPatient(Context.getPatientService()
-								.getPatient(
-										Integer.valueOf(request
-												.getParameter("patientId"))));
-
-				/** Updating status for unmarked ones */
-				for(PatientBill bill: pbList)
-					if(bill.getStatus() == null)
-						PatientBillUtil.markBillAsPaid(bill);
-
-				mav.addObject("policies", InsurancePolicyUtil
-						.getPolicyIdByPatient(Integer.valueOf(request
-								.getParameter("patientId"))));
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		
+		
+		if (request.getParameter("patientId") != null && !request.getParameter("patientId").equals("")
+				&& request.getParameter("dateFrom") != null && !request.getParameter("dateFrom").equals("")
+				&& request.getParameter("dateTo") != null && !request.getParameter("dateTo").equals("")) {
 				
-				mav.addObject("patientBills", pbList);
+			String startDateStr = request.getParameter("dateFrom");
+			String endDateStr = request.getParameter("dateTo");
 
-			} else {
-				request.getSession()
-						.setAttribute(WebConstants.OPENMRS_ERROR_ATTR,
-								"The Patient is not selected. Please select and retry!");
-			}
+			Date startDate = sdf.parse(startDateStr.split("/")[2] + "-"
+					+ startDateStr.split("/")[1] + "-"
+					+ startDateStr.split("/")[0]);
+			
+			Date endDate = sdf.parse(endDateStr.split("/")[2] + "-"
+					+ endDateStr.split("/")[1] + "-"
+					+ endDateStr.split("/")[0]);
+				
+				// List<PatientBill> pbList = PatientBillUtil
+				// .getBillsByPatient(Context.getPatientService()
+				// .getPatient(
+				// Integer.valueOf(request
+				// .getParameter("patientId"))));
+			
+			System.out.println("Start Date: " + startDate + " End Date: " + endDate);
+			
+
+			List<PatientBill> pbList = Context.getService(
+					BillingService.class).billCohortBuilder(null, startDate,
+					endDate, Integer.valueOf(request.getParameter("patientId")), null, null,
+					null);
+
+			/** Updating status for unmarked ones */
+			// for(PatientBill bill: pbList)
+			// if(bill.getStatus() == null)
+			// PatientBillUtil.markBillAsPaid(bill);
+
+			mav.addObject("policies", InsurancePolicyUtil
+					.getPolicyIdByPatient(Integer.valueOf(request
+							.getParameter("patientId"))));
+
+			mav.addObject("patientBills", pbList);
+
+		} else {
+			request.getSession()
+					.setAttribute(WebConstants.OPENMRS_ERROR_ATTR,
+							"The Patient is not selected or date are not filled. Please select and retry!");
+		}
 
 		return mav;
 
