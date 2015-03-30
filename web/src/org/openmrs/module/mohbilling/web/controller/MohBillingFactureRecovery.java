21ad27068cd301911e3c2cec4974eed42045de6c
mariam <mariam@6101c9c6-cf70-4d46-9f71-e9b75042c301>
2015-03-30 15:48:41 +0000

    Revenue report new version---done
    git-svn-id: http://emr.moh.gov.rw/svn/modules/moh-billing/branches/1.6.x@3088 6101c9c6-cf70-4d46-9f71-e9b75042c301

--- web/src/org/openmrs/module/mohbilling/web/controller/MohBillingFactureRecovery.java
+++ web/src/org/openmrs/module/mohbilling/web/controller/MohBillingFactureRecovery.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,8 +18,11 @@ import org.openmrs.Patient;
 import org.openmrs.User;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.mohbilling.businesslogic.InsuranceUtil;
+import org.openmrs.module.mohbilling.model.Beneficiary;
 import org.openmrs.module.mohbilling.model.BillPayment;
 import org.openmrs.module.mohbilling.model.Insurance;
+import org.openmrs.module.mohbilling.model.PatientBill;
+import org.openmrs.module.mohbilling.model.PatientServiceBill;
 import org.openmrs.module.mohbilling.service.BillingService;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.ParameterizableViewController;
@@ -34,7 +38,6 @@ public class MohBillingFactureRecovery extends ParameterizableViewController {
 			HttpServletResponse response) throws Exception {
 
 		List<String> categories = InsuranceUtil.getAllServiceCategories();
-		List<BillPayment> reportedPayments = new ArrayList<BillPayment>();
 
 		ModelAndView mav = new ModelAndView();
 		mav.addObject("allInsurances", InsuranceUtil.getAllInsurances());
@@ -89,6 +92,8 @@ public class MohBillingFactureRecovery extends ParameterizableViewController {
 			String patientNames = null;
 
 			User user = Context.getAuthenticatedUser();
+			Patient patient = null;
+			BillingService service=Context.getService(BillingService.class);
 
 			String cashierNames = (user.getPersonName().getFamilyName() != null ? user
 					.getPersonName().getFamilyName() : "")
@@ -103,7 +108,7 @@ public class MohBillingFactureRecovery extends ParameterizableViewController {
 
 				patientIdStr = request.getParameter("patientId");
 				patientId = Integer.parseInt(patientIdStr);
-				Patient patient = Context.getPatientService().getPatient(
+				patient = Context.getPatientService().getPatient(
 						patientId);
 				patientNames = (patient.getFamilyName() != null ? patient
 						.getFamilyName() : "")
@@ -119,89 +124,41 @@ public class MohBillingFactureRecovery extends ParameterizableViewController {
 				insuranceIdInt = Integer.parseInt(insuranceStr);
 				insurance = InsuranceUtil.getInsurance(insuranceIdInt);
 			}
-
-			List<Date> dates = Context.getService(BillingService.class).getRevenueDates(insurance,
-					startDate, endDate, patientId, serviceId, null,
-					cashCollector);
-
-			Double bigTotal = (double) 0;
 			
-			Map<Date, List<Object[]>> map = new HashMap<Date, List<Object[]>>();
-			Map<String,Double> subTotalMap = new HashMap<String, Double>();
-			
-			BillingService service=Context.getService(BillingService.class);
-			
-			for (Date date : dates) {
-				
-				List<Object[]> totalByCateg = new ArrayList<Object[]>();
-				
-				Double totalConsultation = 0.0;
-				Double totalLabo= 0.0;
-				Double totalFormalite= (double) 0;
-				Double totalRadio= (double) 0;
-				Double totalPediat= (double) 0;
-				Double totalEcho= (double) 0;
-				Double totalOphta= (double) 0;
-				Double totalSurgery= (double) 0;
-				Double totalInternalMed= (double) 0;
-				Double totalGyneco= (double) 0;
-				Double totalKine = (double) 0;
-				Double totalStomato= (double) 0;
-				Double totalPetiteChir= (double) 0;
-				Double totalMaternite= (double) 0;
-				Double totalClinic= (double) 0;
-				Double totalNeo= (double) 0;
-				Double totalAmbulance= (double) 0;
-				Double totalMedic= (double) 0;
-				Double totalConsummable= (double) 0;
-				Double totalMorgue= (double) 0;
-				Double totalAutre= (double) 0;			
-				Double totalOxygenotherapie= (double) 0;
-				Double totalSoinsInf = (double) 0;
-				
 
-				totalConsultation = (Double)service.getSum("CONSULTATION", date);
-				totalLabo=(Double)service.getSum("LABORATOIRE", date);
-				totalFormalite=(Double)service.getSum("FORMALITES ADMINISTRATIVES", date);
-				totalRadio=(Double)service.getSum("RADIOLOGIE", date);
-				totalPediat=(Double)service.getSum("PEDIATRIC", date);
-				totalEcho=(Double)service.getSum("ECHOGRAPHIE", date);
-				totalOphta=(Double)service.getSum("OPHTALMOLOGIE", date);
-				totalSurgery=(Double)service.getSum("CHIRURGIE", date);
-				totalInternalMed=(Double)service.getSum("INTERNAL MEDICINE", date);
-				totalGyneco=(Double)service.getSum("GYNECOLOGY", date);
-				totalKine=(Double)service.getSum("KINESITHERAPIE", date);
-				totalStomato=(Double)service.getSum("STOMATOLOGIE", date);
-				totalMaternite=(Double)service.getSum("MATERNITE", date);
-				totalNeo=(Double)service.getSum("NEONATAL", date);
-				totalMedic=(Double)service.getSum("MEDICAMENTS", date);
-				totalConsummable=(Double)service.getSum("CONSOMMABLES", date);
-				totalOxygenotherapie=(Double)service.getSum("OXYGENOTHERAPIE", date);
-
-				
-				//list of services' revenues
-				totalByCateg.add(new Object[] {totalConsultation,totalLabo,totalFormalite,
-						totalRadio,totalPediat,totalEcho,totalOphta,totalSurgery,
-						totalInternalMed,totalGyneco,totalKine,totalStomato,"",totalMaternite,"",
-						totalNeo,"",totalMedic,totalConsummable,"","" });
-				
-				//this map help to display a date once
-				map.put(date, totalByCateg);
-			}
+			//List<Date> dates = service.getRevenueDates(insurance,startDate, endDate, patientId, serviceId, null,cashCollector);
 			
-			// total of the columns
-			Double colCons = 0.0;
-			for (Date date : map.keySet()) {
-				for (Object[] col : map.get(date)) {
-					if(col[0]!=null)
-					colCons=colCons+(Double)col[0];
+			List<Object[]> obj = new ArrayList<Object[]>();
+			Map<Beneficiary, List<Object[]>> map = new HashMap<Beneficiary, List<Object[]>>();
+			
+			List<PatientBill> bills = service.getBills(startDate, endDate);
+			
+			List<Object[]> totalByCateg = new ArrayList<Object[]>();
+			
+
+			for (PatientBill bill : bills) {
+				
+				Set<PatientServiceBill> billItems = bill.getBillItems();
+				String serviceName = "";
+				
+				
+				Beneficiary beneficiary = bill.getBeneficiary();
+				Patient p = beneficiary.getPatient();
+				
+				for (PatientServiceBill psb : billItems) {
+					
+					
 				}
+					
+				//list of services' revenues
+				
+//				
+//				//this map help to display a date once
+//				map.put(beneficiary, totalByCateg);
 			}
+			mav.addObject("bills", totalByCateg);
+			mav.addObject("obj", obj);
 			mav.addObject("map", map);
-			mav.addObject("subTotalMap", subTotalMap);
-			mav.addObject("bigTotal", bigTotal);
-			
-			mav.addObject("colCons", colCons);
 		}
 
 		mav.setViewName(getViewName());
