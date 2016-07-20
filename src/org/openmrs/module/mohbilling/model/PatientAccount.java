02366a58cc06d0a2cdb48eaf1d4ff0ef812743a9
nmariam <nmary100@gmail.com>
2016-07-20 15:42:57 -0400

    cleaning...

---
+++ src/org/openmrs/module/mohbilling/model/PatientAccount.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package org.openmrs.module.mohbilling.model;
+
+import org.springframework.web.servlet.mvc.ParameterizableViewController;
+
+/**
+ * @author rbcemr
+ *
+ */
+public class PatientAccount extends ParameterizableViewController {
+
+}
