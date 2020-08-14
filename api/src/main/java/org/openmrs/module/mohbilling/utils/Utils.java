f6802df08d83b422e88939f0de63e71be70a3328
Bailly RURANGIRWA <rubailly@gmail.com>
2020-08-14 14:57:43 +0200

    UP-94: Change to using org.slf4j for logging

--- api/src/main/java/org/openmrs/module/mohbilling/utils/Utils.java
+++ api/src/main/java/org/openmrs/module/mohbilling/utils/Utils.java
@@ -3,8 +3,8 @@ package org.openmrs.module.mohbilling.utils;
 import java.util.Arrays;
 import java.util.List;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.openmrs.Concept;
 import org.openmrs.Obs;
 import org.openmrs.Person;
@@ -12,8 +12,8 @@ import org.openmrs.api.context.Context;
 
 public class Utils {
 
+	protected static final Logger log = LoggerFactory.getLogger(Utils.class);	
 	
-	protected static final Log log = LogFactory.getLog(Utils.class);	
 	public static List<Obs> getLastNObservations(java.lang.Integer n, Person who, Concept question, boolean includeVoided) {
 		return Context.getObsService().getObservations(Arrays.asList( who), null, Arrays.asList(question), null, null, null, null, n, null, null, null, includeVoided);
 	}
