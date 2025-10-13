c1ce43047721850754dc5774b21c373cdd2fcb1d
Amos Laboso <alaboso@gmail.com>
2025-10-13 22:47:35 +0300

    Added RoundingMode to BigDecimal - to avoid  scenario

--- api/src/main/java/org/openmrs/module/mohbilling/businesslogic/ThirdPartyBillUtil.java
+++ api/src/main/java/org/openmrs/module/mohbilling/businesslogic/ThirdPartyBillUtil.java
@@ -1,60 +1,62 @@
 /**
- * 
+ *
  */
 package org.openmrs.module.mohbilling.businesslogic;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Date;
 
 import org.openmrs.api.context.Context;
 import org.openmrs.module.mohbilling.model.InsurancePolicy;
 import org.openmrs.module.mohbilling.model.ThirdPartyBill;
 import org.openmrs.module.mohbilling.service.BillingService;
 
-import java.math.BigDecimal;
-import java.util.Date;
-
 /**
  * @author emr
  *
  */
 public class ThirdPartyBillUtil {
-	
-	/**
-	 * Offers the BillingService to be use to talk to the DB
-	 * 
-	 * @return the BillingService
-	 */
-	private static BillingService getService() {
 
-		return Context.getService(BillingService.class);
-	}
+    /**
+     * Offers the BillingService to be use to talk to the DB
+     *
+     * @return the BillingService
+     */
+    private static BillingService getService() {
 
-	public static ThirdPartyBill saveThirdPartyBill(ThirdPartyBill thirdBill) {
-		if (thirdBill != null) {
-			getService().saveThirdPartyBill(thirdBill);
-			
-			return thirdBill;
-		}
+        return Context.getService(BillingService.class);
+    }
 
-		return null;
-	}
-	public static ThirdPartyBill createThirdPartyBill(InsurancePolicy ip,BigDecimal totalAmount){
-		
-		ThirdPartyBill thirdPartyBill = null;
-		
-		if(ip.getThirdParty() != null){
-			 BigDecimal thirdAmount  = totalAmount.multiply(BigDecimal.valueOf((ip.getThirdParty().getRate())/100));
-			
-			 thirdPartyBill = new ThirdPartyBill();
-			 thirdPartyBill.setAmount(thirdAmount);
-			 thirdPartyBill.setCreatedDate(new Date());
-			 thirdPartyBill.setCreator(Context.getAuthenticatedUser());
-			 thirdPartyBill.setVoided(false);
-			
-			 thirdPartyBill = saveThirdPartyBill(thirdPartyBill);
-			
-		}
-		return thirdPartyBill;
-		
-	}
-	
-	
+    public static ThirdPartyBill saveThirdPartyBill(ThirdPartyBill thirdBill) {
+        if (thirdBill != null) {
+            getService().saveThirdPartyBill(thirdBill);
+
+            return thirdBill;
+        }
+
+        return null;
+    }
+
+    public static ThirdPartyBill createThirdPartyBill(InsurancePolicy ip, BigDecimal totalAmount) {
+
+        ThirdPartyBill thirdPartyBill = null;
+
+        if (ip.getThirdParty() != null) {
+            BigDecimal thirdAmount = totalAmount.multiply(BigDecimal.valueOf((ip.getThirdParty().getRate()) / 100)).setScale(0, RoundingMode.HALF_UP);
+
+            thirdPartyBill = new ThirdPartyBill();
+            thirdPartyBill.setAmount(thirdAmount);
+            thirdPartyBill.setCreatedDate(new Date());
+            thirdPartyBill.setCreator(Context.getAuthenticatedUser());
+            thirdPartyBill.setVoided(false);
+
+            thirdPartyBill = saveThirdPartyBill(thirdPartyBill);
+
+        }
+        return thirdPartyBill;
+
+    }
+
+
 }
